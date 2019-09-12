f3744ba3417eb397e522a8bc4fba34a2b85caee1
Aukai Tirrell <aukai.tirrell@gmail.com>
2019-09-11 23:21:26 -0700

    updated and added new pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Layout from "../components/layout";
+import { Layout } from "../components";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => {
+const Home = () => {
   return (
     <Layout>
       <SEO title="Home" />
@@ -17,9 +17,9 @@ const IndexPage = () => {
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/about">Go to page About</Link>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default Home;
---
+++ src/pages/rsvp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Layout } from "../components";
+import SEO from "../components/seo";
+
+const RSVP = () => {
+  return (
+    <Layout>
+      <SEO title="Table RSVP" />
+      <h1>Table RSVP</h1>
+    </Layout>
+  );
+};
+
+export default RSVP;
