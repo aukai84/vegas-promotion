f1a9f63966e1e067c69455b04d48af00ad031b71
Aukai Tirrell <aukai.tirrell@gmail.com>
2019-09-11 21:18:22 -0700

    test netlify audo deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <h1>Testing Netlify Webhook build</h1>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -16,6 +17,6 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
