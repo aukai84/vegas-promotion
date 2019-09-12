0cc436447ef11fa7a2e0954e8b7cf92dc7340874
Aukai Tirrell <aukai.tirrell@gmail.com>
2019-09-11 22:58:34 -0700

    setup eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,21 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>This is the feature branch</h1>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>This is the feature branch</h1>
+      <h1>Hi people</h1>
+      <h2>test</h2>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
 export default IndexPage;
