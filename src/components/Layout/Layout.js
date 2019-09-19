48450e3b4c87eb767120011c991e15fed7aeb117
Aukai Tirrell <aukai.tirrell@gmail.com>
2019-09-19 15:56:53 -0700

    updated layout and added styled components theme

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,8 +8,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-
+import { ThemeProvider } from "styled-components";
 import Header from "../header";
+import { theme, GlobalStyles } from "../../styles";
 import "./index.css";
 
 const Layout = ({ children }) => {
@@ -24,24 +25,22 @@ const Layout = ({ children }) => {
   `);
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <ThemeProvider theme={theme}>
+      <div>
+        <GlobalStyles />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 };
 
