0164bdd4e7922fec076c0b5ee350efd167e96534
Aukai Tirrell <aukai.tirrell@gmail.com>
2020-02-18 22:56:24 -0800

    moved around static assets.  using chakra image and slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "gatsby";
-import { Box } from "@chakra-ui/core";
-import { Layout, Image } from "../components";
+import Slider from "react-slick";
+import { Image } from "@chakra-ui/core";
+import { Layout } from "../components";
 import SEO from "../components/seo";
 
+const CarouselImage = ({ src, alt }) => (
+  <Image src={src} alt={alt} width="100%" height="100vh" objectFit="cover" />
+);
+
+CarouselImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
 const Home = () => {
+  const settings = {
+    infinite: true,
+    fade: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 2200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: "ease-in-out",
+    arrows: false,
+  };
   return (
     <Layout>
       <SEO title="Home" />
-      <Box width="100%" height="100vh">
-        <Image
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-          src="chainsmokers_2.jpg"
-          alt="Chainsmokers"
+      <Slider {...settings}>
+        <CarouselImage src="/resources/chainsmokers_2.jpg" alt="Chainsmokers" />
+        <CarouselImage src="/resources/diplo_1.jpg" alt="Diplo" />
+        <CarouselImage src="/resources/drake-cover.jpg" alt="Drake" />
+        <CarouselImage
+          src="/resources/chainsmokers_2.jpg"
+          alt="Chainsmokers_2"
         />
-      </Box>
+      </Slider>
       <Link to="/about">Go to page About</Link>
     </Layout>
   );
